0076f1ccb28702739b7a7521543b6d793a0e6fa2
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-05-22 16:01:18 +0600

    Added dotenv and deploy in Heroku.

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/test", (req, res) => {
+  res.send("Makers Test Server!");
+});
+
 app.get("/", (req, res) => {
   res.send("Makers Server!");
 });
