94ce106feb94a3b0c9b8083c90d2802925a7296c
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-05-22 15:50:00 +0600

    Basic setup for express server.

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Makers Server!");
+});
+
+app.listen(port, () => {
+  console.log(`Makers Server run at port: ${port}`);
+});
