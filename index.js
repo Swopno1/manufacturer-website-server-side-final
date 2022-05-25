2e4053eecfeecf069f537c3e64ca968dba9b7ca1
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-05-25 19:35:59 +0600

    Added new api to add/put user

--- index.js
+++ index.js
@@ -60,6 +60,27 @@ async function run() {
 
       res.send(cursor);
     });
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '1h' }
+      );
+      res.send({ result, token });
+    });
   } finally {
   }
 }
