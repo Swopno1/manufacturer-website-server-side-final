f604f18674bcbe258daf32a98e3d0d296684ca33
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-05-27 22:44:33 +0600

    Added a couple of API route.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -22,14 +22,14 @@ const client = new MongoClient(uri, {
 function verifyJWT(req, res, nex) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).send({ message: 'un-authorized access' });
+    return res.status(401).send({ message: "un-authorized access" });
   }
 
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
     if (err) {
-      return res.status(403).send({ message: 'forbidden access' });
+      return res.status(403).send({ message: "forbidden access" });
     }
     req.decoded = decoded;
     nex();
@@ -39,9 +39,9 @@ function verifyJWT(req, res, nex) {
 async function run() {
   try {
     await client.connect();
-    const toolsCollection = client.db('makers').collection('tools');
-    const usersCollection = client.db('makers').collection('users');
-    const ordersCollection = client.db('makers').collection('orders');
+    const toolsCollection = client.db("makers").collection("tools");
+    const usersCollection = client.db("makers").collection("users");
+    const ordersCollection = client.db("makers").collection("orders");
 
     // *** User & Admin related Routes ***
     /**
@@ -53,26 +53,26 @@ async function run() {
         email: requester,
       });
 
-      if (requestAccount.role === 'admin') {
+      if (requestAccount.role === "admin") {
         next();
       } else {
-        res.status(403).send({ message: 'forbidden access' });
+        res.status(403).send({ message: "forbidden access" });
       }
     };
     /**
      * Get admin
      */
-    app.get('/admin/:email', async (req, res) => {
+    app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
       const user = await usersCollection.findOne({ email: email });
-      const isAdmin = user.role === 'admin';
+      const isAdmin = user.role === "admin";
 
       res.send({ admin: isAdmin });
     });
     /**
      * Register New User
      */
-    app.put('/user/:email', async (req, res) => {
+    app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
       const filter = { email: email };
@@ -88,13 +88,13 @@ async function run() {
       const token = jwt.sign(
         { email: email },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: '1h' }
+        { expiresIn: "1h" }
       );
       res.send({ result, token });
     });
 
     // Find All tools
-    app.get('/tools', async (req, res) => {
+    app.get("/tools", async (req, res) => {
       const query = {};
       const cursor = await toolsCollection.find(query);
       const tools = await cursor.toArray();
@@ -103,7 +103,7 @@ async function run() {
     });
 
     // Find a single tool
-    app.get('/tools/:id', async (req, res) => {
+    app.get("/tools/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const cursor = await toolsCollection.findOne(query);
@@ -111,8 +111,33 @@ async function run() {
       res.send(cursor);
     });
 
+    // Add new Tool
+    app.post("/tool", verifyJWT, verifyAdmin, async (req, res) => {
+      const product = req.body;
+      const result = await toolsCollection.insertOne(product);
+
+      res.send({ success: true, result });
+    });
+
+    // Update a tool
+    app.put("/tool/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedTool = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: updatedTool,
+      };
+      const result = await toolsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send({ success: true, result });
+    });
+
     // Purchase/Order Related Route
-    app.post('/purchase', async (req, res) => {
+    app.post("/purchase", async (req, res) => {
       const product = req.body;
       const result = await ordersCollection.insertOne(product);
 
@@ -124,7 +149,7 @@ async function run() {
       res.send({ success: true, result });
     });
 
-    app.get('/allorders', async (req, res) => {
+    app.get("/allorders", async (req, res) => {
       const query = {};
       const cursor = await ordersCollection.find(query);
       const orders = await cursor.toArray();
@@ -132,14 +157,14 @@ async function run() {
       res.send(orders);
     });
 
-    app.delete('/myorders/:id', async (req, res) => {
+    app.delete("/myorders/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
       const result = await ordersCollection.deleteOne(filter);
       res.send({ success: true, result });
     });
 
-    app.put('/myorders/:id', async (req, res) => {
+    app.put("/myorders/:id", async (req, res) => {
       const id = req.params.id;
       const updatedOrder = req.body;
       const filter = { _id: ObjectId(id) };
@@ -155,7 +180,7 @@ async function run() {
       res.send({ success: true, result });
     });
 
-    app.get('/myorders/:email', async (req, res) => {
+    app.get("/myorders/:email", async (req, res) => {
       const email = req.params.email;
       const query = { user: email };
       const cursor = await ordersCollection.find(query).toArray();
@@ -182,8 +207,8 @@ async function run() {
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Makers Server!');
+app.get("/", (req, res) => {
+  res.send("Makers Server!");
 });
 
 app.listen(port, () => {
